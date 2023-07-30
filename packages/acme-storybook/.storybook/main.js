2f78b392fc8b414e317413b243e57dc08f109b10
Thomas <thomasahervey@gmail.com>
2023-07-29 22:37:43 -0400

    bumped: turbo, storybook

--- packages/acme-storybook/.storybook/main.js
+++ packages/acme-storybook/.storybook/main.js
@@ -1,28 +1,30 @@
-const path = require('path')
+import { dirname, join } from "path";
+const path = require('path');
 module.exports = {
-  stories: [
-    {
-      directory: '../../acme-design-system/src/**',
-      files: '*.stories.*',
-      titlePrefix: 'Design System',
-    },
-  ],
-  addons: [
-    // https://storybook.js.org/addons/@storybook/addon-links
-    '@storybook/addon-links',
-    // https://storybook.js.org/docs/react/essentials/introduction
-    '@storybook/addon-essentials',
-    '@storybook/addon-styling',
-  ],
+  stories: [{
+    directory: '../../acme-design-system/src/**',
+    files: '*.stories.*',
+    titlePrefix: 'Design System'
+  }],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-styling")],
   framework: {
-    name: '@storybook/react-vite',
-    options: {},
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {}
   },
-  async viteFinal(config, { configType }) {
+  async viteFinal(config, {
+    configType
+  }) {
     // customize the Vite config here
-    return config
+    return config;
   },
   docs: {
-    autodocs: true,
-  },
+    autodocs: true
+  }
+};
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
 }
